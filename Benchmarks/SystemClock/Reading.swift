a733c6ef110284de9c6dc95c25526591934b1047
MahdiBM <github@mahdibm.com>
2026-08-31 23:49:31 +0330

    Bench adjustments

--- Benchmarks/SystemClock/Reading.swift
+++ Benchmarks/SystemClock/Reading.swift
@@ -57,27 +57,27 @@ let readingBenchmarks: @Sendable () -> Void = {
     // MARK: - Reading_Continuous
 
     Benchmark(
-        "Reading_Continuous_CPU_16M",
+        "Reading_Continuous_CPU_4M",
         configuration: .init(
             metrics: [.cpuUser],
             warmupIterations: 5,
             maxIterations: 1000
         )
     ) { benchmark in
-        for _ in 0..<16_000_000 {
+        for _ in 0..<4_000_000 {
             blackHole(SystemClock<CompactDuration>.systemContinuous.now)
         }
     }
 
     Benchmark(
-        "Reading_Continuous_Stdlib_CPU_8M",
+        "Reading_Continuous_Stdlib_CPU_4M",
         configuration: .init(
             metrics: [.cpuUser],
             warmupIterations: 5,
             maxIterations: 1000
         )
     ) { benchmark in
-        for _ in 0..<8_000_000 {
+        for _ in 0..<4_000_000 {
             blackHole(ContinuousClock.now)
         }
     }
@@ -94,6 +94,17 @@ let readingBenchmarks: @Sendable () -> Void = {
     }
 
     Benchmark(
+        "Reading_Continuous_Stdlib_Malloc",
+        configuration: .init(
+            metrics: [.mallocCountTotal],
+            warmupIterations: 1,
+            maxIterations: 10
+        )
+    ) { benchmark in
+        blackHole(ContinuousClock.now)
+    }
+
+    Benchmark(
         "Reading_Continuous_Instructions_10K",
         configuration: .init(
             metrics: [.instructions],
@@ -103,6 +114,19 @@ let readingBenchmarks: @Sendable () -> Void = {
     ) { benchmark in
         for _ in 0..<10_000 {
             blackHole(SystemClock<CompactDuration>.systemContinuous.now)
+        }
+    }
+
+    Benchmark(
+        "Reading_Continuous_Stdlib_Instructions_10K",
+        configuration: .init(
+            metrics: [.instructions],
+            warmupIterations: 1,
+            maxIterations: 10
+        )
+    ) { benchmark in
+        for _ in 0..<10_000 {
+            blackHole(ContinuousClock.now)
         }
     }
 
@@ -135,27 +159,27 @@ let readingBenchmarks: @Sendable () -> Void = {
     // MARK: - Reading_Suspending
 
     Benchmark(
-        "Reading_Suspending_CPU_16M",
+        "Reading_Suspending_CPU_4M",
         configuration: .init(
             metrics: [.cpuUser],
             warmupIterations: 5,
             maxIterations: 1000
         )
     ) { benchmark in
-        for _ in 0..<16_000_000 {
+        for _ in 0..<4_000_000 {
             blackHole(SystemClock<CompactDuration>.systemSuspending.now)
         }
     }
 
     Benchmark(
-        "Reading_Suspending_Stdlib_CPU_8M",
+        "Reading_Suspending_Stdlib_CPU_4M",
         configuration: .init(
             metrics: [.cpuUser],
             warmupIterations: 5,
             maxIterations: 1000
         )
     ) { benchmark in
-        for _ in 0..<8_000_000 {
+        for _ in 0..<4_000_000 {
             blackHole(SuspendingClock.now)
         }
     }
@@ -172,6 +196,17 @@ let readingBenchmarks: @Sendable () -> Void = {
     }
 
     Benchmark(
+        "Reading_Suspending_Stdlib_Malloc",
+        configuration: .init(
+            metrics: [.mallocCountTotal],
+            warmupIterations: 1,
+            maxIterations: 10
+        )
+    ) { benchmark in
+        blackHole(SuspendingClock.now)
+    }
+
+    Benchmark(
         "Reading_Suspending_Instructions_10K",
         configuration: .init(
             metrics: [.instructions],
@@ -181,6 +216,19 @@ let readingBenchmarks: @Sendable () -> Void = {
     ) { benchmark in
         for _ in 0..<10_000 {
             blackHole(SystemClock<CompactDuration>.systemSuspending.now)
+        }
+    }
+
+    Benchmark(
+        "Reading_Suspending_Stdlib_Instructions_10K",
+        configuration: .init(
+            metrics: [.instructions],
+            warmupIterations: 1,
+            maxIterations: 10
+        )
+    ) { benchmark in
+        for _ in 0..<10_000 {
+            blackHole(SuspendingClock.now)
         }
     }
 
